616e07e4068b6f2ae1c33d41bd10df8bbcee6bc5
rigon <2412789408@qq.com>
2025-01-31 19:56:20 +0800

    链式操作与更新删除

--- session/record_test.go
+++ session/record_test.go
@@ -1,6 +1,8 @@
 package session
 
-import "testing"
+import (
+	"testing"
+)
 
 var (
 	user1 = &User{"Tom", 18}
@@ -33,5 +35,65 @@ func TestSession_Find(t *testing.T) {
 	var users []User
 	if err := s.Find(&users); err != nil || len(users) != 2 {
 		t.Fatal("failed to query all")
+	}
+}
+
+func TestSession_First(t *testing.T) {
+	s := testRecordInit(t)
+	u := &User{}
+	err := s.First(u)
+	if err != nil || u.Name != "Tom" || u.Age != 18 {
+		t.Fatal("failed to query first")
+	}
+}
+
+func TestSession_Limit(t *testing.T) {
+	s := testRecordInit(t)
+	var users []User
+	err := s.Limit(1).Find(&users)
+	if err != nil || len(users) != 1 {
+		t.Fatal("failed to query with limit condition")
+	}
+}
+
+func TestSession_Where(t *testing.T) {
+	s := testRecordInit(t)
+	var users []User
+	_, err1 := s.Insert(user3)
+	err2 := s.Where("Age = ?", 25).Find(&users)
+
+	if err1 != nil || err2 != nil || len(users) != 2 {
+		t.Fatal("failed to query with where condition")
+	}
+}
+
+func TestSession_OrderBy(t *testing.T) {
+	s := testRecordInit(t)
+	u := &User{}
+	err := s.OrderBy("Age DESC").First(u)
+
+	if err != nil || u.Age != 25 {
+		t.Fatal("failed to query with order by condition")
+	}
+}
+
+func TestSession_Update(t *testing.T) {
+	s := testRecordInit(t)
+	affected, _ := s.Where("Name = ?", "Tom").Update("Age", 30)
+	u := &User{}
+	_ = s.OrderBy("Age DESC").First(u)
+
+	if affected != 1 || u.Age != 30 {
+		t.Fatal("failed to update record")
+	}
+}
+
+func TestSession_DeleteAndCount(t *testing.T) {
+	s := testRecordInit(t)
+	affected, _ := s.Where("Name = ?", "Tom").Delete()
+	count, _ := s.Count()
+
+	if affected != 1 || count != 1 {
+		t.Fatal("failed to delete or count")
 	}
 }
